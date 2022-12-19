9b2b93928196f7abf4b95f027bc58a358be2a5a3
Donal Shortt <donalshortt@donals.eu>
2022-12-19 15:59:00 +0100

    latest

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -4,6 +4,6 @@ DIR=`dirname $0`
 cd $DIR/omnetpp-vnc
 docker build . -t gabrielmoreno/omnetpp-vnc:5.4.1
 cd ..
-docker build . -t gabrielmoreno/swim:1.0.1
+docker build . -t kemalsheker/swim:1.0.1
 cd $CURDIR
 
